5dd8614481eb7f9e34a64d317f8105625f67704a
Diego <dflores7237@gmail.com>
2020-02-26 14:25:56 -0800

    Console log each log entry before pushing to csv file. Display log data in JSON format when a user hits the /logs route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-// const logFile = require('./log.csv')
 const fs = require('fs');
+const csv = require('csvtojson');
+const csvFilePath = './server/log.csv'
 const app = express();
+
 
 app.use((req, res, next) => {
 // write your logging code here
@@ -9,38 +11,43 @@ const agent = req.headers["user-agent"].replace(/,/g, '');
 const time = new Date().toISOString();
 const method = req.method;
 const resource = req.url;
-const version = `http/${req.httpVersion}`;
+const version = `HTTP/${req.httpVersion}`;
 const status = '200';
 
-fs.appendFile('./server/log.csv', agent+",", (err) => {
+fs.appendFile(csvFilePath, agent+",", (err) => {
     if (err) throw err;
-    fs.appendFile('./server/log.csv', time+",", (err) => {
+    fs.appendFile(csvFilePath, time+",", (err) => {
         if (err) throw err;
-        fs.appendFile('./server/log.csv', method+",", (err) => {
+        fs.appendFile(csvFilePath, method+",", (err) => {
                 if (err) throw err;
-                fs.appendFile('./server/log.csv', resource+",", (err) => {
+                fs.appendFile(csvFilePath, resource+",", (err) => {
                     if (err) throw err;
-                    fs.appendFile('./server/log.csv', version+",", (err) => {
+                    fs.appendFile(csvFilePath, version+",", (err) => {
                         if (err) throw err;
-                        fs.appendFile('./server/log.csv', status+"\n", (err) => {
+                        fs.appendFile(csvFilePath, status+"\n", (err) => {
                             if (err) throw err;
+                            console.log(`${agent},${time},${method},${resource},${version},${status}`)
+                            next();
 })})})})})});
-next();
-
 });
 
 app.get('/', (req, res) => {
-    console.log(req);
 // write your code to respond "ok" here
     res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
-    console.log(req);
+
 // write your code to return a json object containing the log data here
-    fs.readFile('./server/log.csv', (err,data) => {
-        res.status(200).send(data)
-    });
+    csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        res.send(jsonObj);
+        console.log(jsonObj);
+    })
+    // fs.readFile('./server/log.csv', (err,data) => {
+    //     res.status(200).send(data);
+    // });
 });
 
 module.exports = app;
